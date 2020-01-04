a05051a43699fbd3f07fd5406b006f148e8d93f7
ducnguyen948 <levinguyen.dl@gmail.com>
2020-01-04 11:30:22 +0700

    Add color segment.

--- commons.py
+++ commons.py
@@ -116,6 +116,16 @@ def rotate_remain_bound(image, angle, mode=None, show=False):
 
     return rotated
 
+def multiple_color_spaces(image):
+    RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    LAB = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    cv2.imshow("BGR", image)
+    cv2.imshow("RGB", RGB)
+    cv2.imshow("HSV", HSV)
+    cv2.imshow("LAB", LAB)
+    cv2.waitKey(0)
+    
 def rotate_by_angle(image, angle, mode=None, show=False):
     # grab the dimensions of the image
     (h, w) = image.shape[:2]
@@ -146,3 +156,61 @@ def rotate_by_angle(image, angle, mode=None, show=False):
             cv2.imshow("rotate_by_angle", rotated)
 
     return rotated
+
+def color_segment(image, color_space="RGB", mode=None, show=False):
+
+    transformed = image.copy()
+
+    blured = cv2.medianBlur(transformed, 21)
+    mask = cv2.inRange(blured, (100, 0, 0), (255, 255, 255))
+    segmented = cv2.bitwise_and(image, image, mask=mask)
+    
+    if show == True:
+        cv2.imshow("blured", blured)
+        cv2.imshow("mask", mask)
+        cv2.imshow("segmented", segmented)
+        cv2.waitKey(0)
+
+    if mode == "TP":
+        cv2.namedWindow("mask", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('mask', 600,600)
+
+        cv2.createTrackbar("space", "mask", 0,3,nothing)
+
+        cv2.createTrackbar("blur", "mask", 10,30,nothing) 
+        cv2.createTrackbar("l1", "mask",0,255,nothing)  
+        cv2.createTrackbar("l2", "mask",0,255,nothing)      
+        cv2.createTrackbar("l3", "mask",0,255,nothing)  
+        cv2.createTrackbar("h1", "mask",0,255,nothing)  
+        cv2.createTrackbar("h2", "mask",0,255,nothing)      
+        cv2.createTrackbar("h3", "mask",0,255,nothing)
+
+        while(1):
+
+            if cv2.waitKey(1) == ord("q"):
+                cv2.destroyAllWindows()
+            
+            space = cv2.getTrackbarPos("space", "mask")
+
+            if space == 1:
+                transformed = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            elif space == 2:
+                transformed = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            elif space == 3:
+                transformed = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+
+            b=cv2.getTrackbarPos("blur", "mask")
+            l1=cv2.getTrackbarPos("l1", "mask")
+            l2=cv2.getTrackbarPos("l2", "mask")
+            l3=cv2.getTrackbarPos("l3", "mask")
+            h1=cv2.getTrackbarPos("h1", "mask")
+            h2=cv2.getTrackbarPos("h2", "mask")
+            h3=cv2.getTrackbarPos("h3", "mask")
+
+            blured = cv2.medianBlur(transformed, 2*b+1)
+            # cv2.imshow("blured", blured)
+            mask = cv2.inRange(blured, (l1, l2, l3), (h1, h2, h3))
+            cv2.imshow("mask", mask)
+            segmented = cv2.bitwise_and(image, image, mask=mask)
+            cv2.imshow("segmented", segmented)
+    return mask
--- demo.py
+++ demo.py
@@ -1,16 +1,22 @@
 import cv2
 import numpy as np
 
-from commons import rotate_remain_bound
+from commons import color_segment
+
+def nothing():
+    pass
 
 if __name__ == "__main__":
     image = cv2.imread("images/idcard.png")
-    cv2.imshow("image", image)
-    cv2.waitKey(0)
+    image = cv2.resize(image, (640, 480))
+    # cv2.imshow("image", image)
+    # cv2.waitKey(0)
 
     # resized = resize_by_size(image, (300, 300), show=True)
     # resized = resize_by_factor(image, 0.5, 0.5, show=True)
     # resized = resize_by_factor(image, 0.5, 0.5, mode="TP")
     # rotated = rotate_by_direction(image, direction="L", show=True)
     # rotated = rotate_by_angle(image, 3, mode="TP")
-    rotated = rotate_remain_bound(image, -3, mode="TP")
+    # rotated = rotate_remain_bound(image, -3, mode="TP")
+
+    color_segment(image, mode="TP")
